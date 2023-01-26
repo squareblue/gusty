52391c5127bd9bb0e873f3d1d595b12c63199742
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-01-26 14:41:08 -0600

    Updates for Square Blue fork

--- src/js/app.js
+++ src/js/app.js
@@ -7,15 +7,15 @@ import { footer } from './components/footer';
 export function app(selector) {
   document.querySelector(selector).insertAdjacentHTML('beforeend', footer(`
 <p class="mt-4">
-&copy; 2023 Mulletech 
+&copy; 2023 Square Blue 
 <span class="px-4">|</span>
 ${link({ 
-    href: 'https://mulle.tech', 
+    href: 'https://squareblue.dev', 
     'class': 'underline' 
 })}
 <span class="px-4">|</span>
 ${link({ 
-    href: 'https://github.com/mulletech/gusty/', 
+    href: 'https://github.com/squareblue/gusty/', 
     'class': 'underline' 
   }, 'GitHub')}
 </p>
