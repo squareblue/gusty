9572401619af03afa7b3228771271c865a057797
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-02-01 11:38:00 -0600

    Added comments to 'main.js'.

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,10 @@ import { app } from './app';
 console.log('Gusty!');
 document.title = 'Hello from Gusty!';
 
-// Hydrate 'dynamic' content into #root element
+// Hydrate 'dynamic' content into specified element
+// You can use a selector string or a reference to
+// an existing DOM node. For an SPA that is completely
+// rendered dynamically, you'd typically have one DOM
+// element on the page (conventionally with the id 'root')
+// and use that for your render target.
 app('#footer-container');
